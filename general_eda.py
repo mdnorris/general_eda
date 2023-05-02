04d46fe23d2b45d8bb591c86e83f9ba0c978c0b2
Matthew Norris <matthew@mdnorris.com>
2023-05-02 03:36:04 -0500

    Commit before changing interpreter

--- general_eda.py
+++ general_eda.py
@@ -2,10 +2,13 @@ import os
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+# %matplotlib inline
 import seaborn as sns
+from jedi.api.refactoring import inline
+from collections import defaultdict
 sns.set_style('darkgrid')
-from rfpimp import *
-from rfpimp import plot_corr_heatmap
+# from rfpimp import *
+# from rfpimp import plot_corr_heatmap
 from sklearn.ensemble import RandomForestRegressor, RandomForestClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
@@ -19,48 +22,20 @@ import category_encoders as ce
 from sklearn.preprocessing import StandardScaler
 from multiprocessing import Pool
 from sklearn.inspection import permutation_importance
-import category_encoders as ce
 from sklearn import preprocessing
 import shap
 # import mkl
+from jinja2 import escape
+from jinja2 import Markup
 from statsmodels.stats.outliers_influence import variance_inflation_factor
 from fitter import Fitter, get_common_distributions, get_distributions
 import xarray
 from sklearn.metrics import mean_squared_error
-from autoviz.AutoViz_Class import AutoViz_Class
-AV = AutoViz_Class()
 import dtale
 import time
 
 
-def timing_decorator(func):
-    def wrapper(*args, **kwargs):
-        start_time = time.time()
-        result = func(*args, **kwargs)
-        end_time = time.time()
-        print(f"Function {func.__name__} took {end_time - start_time} seconds to run.")
-        return result
-
-    return wrapper
-
-
-print(os.getcwd())
-
-df = pd.read_csv('C:/Users/norri/Documents/GitHub/mercury-ds/attribution/cortex_Push.csv')
-df.describe()
-df.info()
-
-
-float = df.select_dtypes(include='float64')
-int = df.select_dtypes(include='int64')
-object = df.select_dtypes(include='object')
-
-dtale.show(df)
-
-int_float = pd.concat([float, int], axis=1)
-
-
-def missing_values(df):
+def missing_values(data):
     names = [var for var in int_float.columns]
     missing_count = df[names].isnull().sum()
     var_count = np.array(df[names].isnull().sum() * 100 / len(df)).round(2)
@@ -70,46 +45,54 @@ def missing_values(df):
     print(missing)
 
 
-    def unique(df):
-        percent_unique = np.array(100 * df.nunique() / len(df.index)).round(2)
-        count_unique = df.nunique()
-        names = [var for var in df.columns]
-        unique_df = pd.DataFrame(index=names)
-        unique_df["Count Unique"] = count_unique
-        unique_df["Percent Unique"] = percent_unique
-        print(unique_df)
+def unique(data):
+    percent_unique = np.array(100 * df.nunique() / len(df.index)).round(2)
+    count_unique = df.nunique()
+    names = [var for var in df.columns]
+    unique_df = pd.DataFrame(index=names)
+    unique_df["Count Unique"] = count_unique
+    unique_df["Percent Unique"] = percent_unique
+    print(unique_df)
 
 
-corr_temp = int_float
-corr_names = corr_temp.columns.tolist()
-temp_df = int_float[corr_names]
-corr = temp_df.corr(method="pearson").round(2)
-mask = np.triu(np.ones_like(corr, dtype=bool))
-f, ax = plt.subplots(figsize=(18, 18))
-cmap = sns.diverging_palette(250, 1, as_cmap=True)
-corr_plot = sns.heatmap(corr, annot=True, mask=mask, cmap=cmap,
-            vmax=1, vmin=-1, center=0,
-            square=True, linewidths=.5, cbar_kws={"shrink": .5})
-plt(corr_plot)
-
-
-def distributions(df):
+counts = defaultdict(int)
+def distributions(data):
     for var in int_float:
         col_names = list(int_float.columns.values)
         dist_test = int_float.dropna().to_numpy()
         dist_list = ['gamma', 'expon', 'cauchy', 'norm', 'uniform']
         f = Fitter(dist_test, distributions=dist_list, timeout=60)
-        f.fit()
-        # print(var)
-        f.summary(plot=True)
-        f.get_best(method='sumsquare_error')
+        print(f.fit())
+        counts[1] += 1
+        if counts[2] < 39:
+            counts[2] = 39
+        else:
+            counts[1] = 38
+        print(col_names[counts[1] - 1:counts[2]])
+        print(f.summary(plot=True))
+        print(f.get_best(method='sumsquare_error'))
 
 
-    def summary(df):
-        print(missing_values(df))
-        print(unique(df))
+def explanation(data):
+    print(missing_values(data))
+    print(unique(data))
 
 
+df = pd.read_csv('C:/Users/norri/Documents/GitHub/mercury-ds/attribution/cortex_Push.csv')
+df.describe()
+df.info()
+
+floater = df.select_dtypes(include='float64')
+integ = df.select_dtypes(include='int64')
+obj = df.select_dtypes(include='object')
+
+# dtale.show(df)
+
+int_float = pd.concat([floater, integ], axis=1)
+
+corr = int_float.corr(method="pearson")
+corr.style.background_gradient(cmap="coolwarm")
+sns.heatmap(corr)
+
+explanation(int_float)
 distributions(int_float)
-summary(int_float)
-print(corr_plot(int_float))
